19fba36b639cc04747b6637e56b00636c86f4203
Laszlo Korte <me@laszlokorte.de>
2012-05-20 01:08:42 +0200

    make juggernaut connection secure

--- static/lugcampkasse.js
+++ static/lugcampkasse.js
@@ -1,7 +1,7 @@
 (function() {
   var global = this;
 
-  var jug = new Juggernaut({secure:false});
+  var jug = new Juggernaut({secure:true});
 
   var lib = global.lugcampkasse = {
     urlRoot : '/',
